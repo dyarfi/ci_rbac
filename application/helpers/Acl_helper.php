b11b52177aef28146ede7fb0061170f21498c5f4
defrianyarfi <defrianyarfi@10.165.85.122>
2014-12-10 17:19:03 +0700

    Changes Updated

--- application/helpers/Acl_helper.php
+++ application/helpers/Acl_helper.php
@@ -1,67 +1,51 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-Class ACL extends CI_Controller {
-				
+class ACL {
+		
+	public $CI;
 	public $user = '';
-	public $privileges;
-	
-	public $module_list;
-	public $module_function_list;
-	public $module_request;
-	public $module_menu;
-	
-	public $previous_url;
+	public $previous_url = '';
+	public $session = '';
 	
 	public function __construct () {
-		parent::__construct();	
-		
-		$this->CI =& get_instance();
-						
-		$this->user	= self::user();						
-		
+		parent::__construct();		
+				
 		if (strpos($this->session->userdata('prev_url'), ADMIN) !== FALSE 
 				&& $this->session->userdata('prev_url') != $this->session->userdata('curr_url')) {
 			// Set Previous URL to current URL
 			$this->session->set_userdata('prev_url', $this->session->userdata('curr_url'));
-		}	else {
+		} else {
 			// Set current URL from current url
 			$this->session->set_userdata('curr_url', $this->uri->uri_string());
 		}
 		
-		if ($this->session->userdata('user_session') == '') {
-			// Redirect if not logged
-			redirect(ADMIN);
-		}
-		
+		$this->user		= self::user();			
+	
 		// Set previous URL from previous url session		
-		$this->previous_url	= $this->session->userdata('prev_url');				
-							
+		$this->previous_url	= $this->session->userdata('prev_url');
+									
 	}
 	
+	/**
+	* Load the current users data
+	*
+	* @access	public
+	* @param	NULL (session)
+	* @return	array object
+	*/		
 	public function user() {
-		
-		$user = $this->session->userdata('user_session');
-		
-		$this->user = '';
-		
-		if($user) {
-			$this->user = (object) $user;
-		} 
+				
+		$this->user		= $this->session->userdata('user_session');
 		
 		// Return user data result from session
 		return $this->user;
-	}
-	
-	public function previous_url() {
-		// Return previous url from session
-		return ($this->previous_url) ? $this->previous_url : '';
 	}
 
 	/**
 	* Load the current users available module list
 	*
 	* @access	public
-	* @param	array
+	* @param	NULL (session)
 	* @return	array
 	*/	
 	public function admin_system_modules () {
@@ -75,8 +59,6 @@ Class ACL extends CI_Controller {
 			//$this->userhistory		= Model_UserHistory::instance();
 			$this->module_list			= json_decode($this->session->userdata('module_list'),TRUE);
 			$this->module_function_list	= json_decode($this->session->userdata('module_function_list'),TRUE);
-			//$this->module_request		= $this->uri->uri_string;	
-			//$this->module_menu			= self::check_module_menu($this->module_request);
 		}
 		
 		$modules				= array();
@@ -90,6 +72,10 @@ Class ACL extends CI_Controller {
 		return $modules;		
 	}
 	
+	
+	public function isAuthorized() {
+
+	}
 	
 }
 
